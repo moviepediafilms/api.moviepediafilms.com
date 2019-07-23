18abb0191cd8cba11d76fbfaa614f04d173f6956
Zeeshan Khan <zkhan1093@gmail.com>
2019-07-24 00:18:23 +0530

    adding staticroot

--- moviepediac/settings.py
+++ moviepediac/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'staticroot'
 
 # if 'HEROKU' in os.environ:
 #     import django_heroku
