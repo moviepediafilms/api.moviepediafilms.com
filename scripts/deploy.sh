c97f20063f2dad0357034d99c6e4f0b20ae2a42d
Zeeshan Khan <zkhan1093@gmail.com>
2020-10-06 20:44:14 +0530

    deploy script from local

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,33 +1,23 @@
 #! /bin/bash
-echo "changing directory" | systemd-cat
-cd /home/zeeshan/api.moviepediafilms.com
-echo $(pwd) | systemd-cat
+# script to deploy api.moviepediafilms.com from local system
+# ssh keys should be set up already
 
-echo "git pull" | systemd-cat
-/usr/bin/git pull
-echo "ok" | systemd-cat
+# variable used
+SERVER=moviepediafilms.com
+WORKING_DIR=/home/zeeshan/api.moviepediafilms.com
+USER=zeeshan
+# commands user
+pipenv=/home/zeeshan/.local/bin/pipenv
+git=/usr/bin/git
+ssh=/usr/bin/ssh
+service=/usr/sbin/service
 
-echo "pipenv sync" | systemd-cat
-/home/zeeshan/.local/bin/pipenv sync
-echo "ok" | systemd-cat
-echo "pipenv clean" | systemd-cat
-/home/zeeshan/.local/bin/pipenv clean
-echo "ok" | systemd-cat
-
-echo "pipenv run python manage.py check --deploy" | systemd-cat
-/home/zeeshan/.local/bin/pipenv run python manage.py check --deploy
-echo "ok" | systemd-cat
-
-# pipenv run python manage.py migrate --noinput
-
-echo "pipenv run python manage.py collectstatic --noinput" | systemd-cat
-/home/zeeshan/.local/bin/pipenv run python manage.py collectstatic --noinput
-echo "ok" | systemd-cat
-
-echo "pipenv run python manage.py migrate --noinput" | systemd-cat
-/home/zeeshan/.local/bin/pipenv run python manage.py migrate --noinput
-echo "ok" | systemd-cat
-
-echo "sudo service api.moviepediafilms restart" | systemd-cat
-sudo /usr/sbin/service api.moviepediafilms restart
-echo "ok" | systemd-cat
+$ssh $USER@$SERVER "
+cd $WORKING_DIR &&
+echo $git pull; $git pull &&
+echo $pipenv clean; $pipenv clean &&
+echo $pipenv sync; $pipenv sync &&
+echo $pipenv run python manage.py check --deploy; $pipenv run python manage.py check --deploy &&
+echo $pipenv run python manage.py collectstatic --noinput; $pipenv run python manage.py collectstatic --noinput &&
+echo $pipenv run python manage.py migrate --noinput; $pipenv run python manage.py migrate --noinput &&
+echo sudo $service api.moviepediafilms restart; sudo $service api.moviepediafilms restart"
