4ed6ff56a232a6f95cdd22ace16cd69a66670139
Zeeshan Khan <zkhan1093@gmail.com>
2020-12-12 21:10:32 +0530

    fix sendgrid email using dynamic templates

--- backends/sendgrid.py
+++ backends/sendgrid.py
@@ -38,33 +38,41 @@ class SendgridEmailBackend(BaseEmailBackend):
                 logger.debug(f"sending mail {sg_mail}")
                 if self.dry_run:
                     logger.debug("mail not sent! it was a dry run")
+                    self.sg.client.mail.send.post(request_body=sg_mail)
                 else:
                     self.sg.client.mail.send.post(request_body=sg_mail)
                 count += 1
-            except Exception:
+            except Exception as ex:
+                logger.error(
+                    f"Sendgrid error: {getattr(ex, 'body', b'<no-content>').decode()}"
+                )
                 if not self.fail_silently:
                     raise
         return count
 
     def _build_sg_mail(self, email):
-        personalization = {"to": [], "cc": [], "bcc": [], "substitutions": {}}
-        sendgrid_mail = {
-            "from": self._process_email_addr(email.from_email),
-            "reply_to": self._process_email_addr(email.reply_to),
-            "subject": email.subject,
-            "personalizations": [personalization],
-        }
-        for e in email.to:
-            personalization["to"].append(self._process_email_addr(e))
-        for e in email.cc:
-            personalization["cc"].append(self._process_email_addr(e))
-        for e in email.bcc:
-            personalization["bcc"].append(self._process_email_addr(e))
+        personalization = {}
+        sendgrid_mail = {}
+
+        if email.from_email:
+            sendgrid_mail["from"] = self._process_email_addr(email.from_email)
+        if email.subject:
+            sendgrid_mail["subject"] = (email.subject,)
+        if email.to:
+            personalization["to"] = [self._process_email_addr(e) for e in email.to]
+        if email.cc:
+            personalization["cc"] = [self._process_email_addr(e) for e in email.cc]
+        if email.bcc:
+            personalization["bcc"] = [self._process_email_addr(e) for e in email.bcc]
+        if email.reply_to:
+            sendgrid_mail["reply_to"] = self._process_email_addr(email.reply_to)
 
         if hasattr(email, "template_id"):
             sendgrid_mail["template_id"] = email.template_id
-            if hasattr(email, "substitutions"):
-                personalization["substitutions"].update(email.substitutions)
+            if hasattr(email, "template_data"):
+                personalization["dynamic_template_data"].update(email.template_data)
+        if personalization:
+            sendgrid_mail["personalizations"] = [personalization]
         return sendgrid_mail
 
     def _process_email_addr(self, email_addr):
