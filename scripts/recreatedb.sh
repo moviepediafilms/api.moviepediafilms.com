64d89385bc32eb14333db5805b83b75374bd1a7a
Zeeshan Khan <zkhan1093@gmail.com>
2020-12-20 01:41:02 +0530

    0001 is final now

--- scripts/recreatedb.sh
+++ scripts/recreatedb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 export WORKSPACE=/home/zeeshan/repos/api.moviepediafilms.com
 cd $WORKSPACE
-rm $WORKSPACE/api/migrations/0001_initial.py
+# rm $WORKSPACE/api/migrations/0001_initial.py
 pipenv run python manage.py makemigrations
 pipenv run rm $WORKSPACE/db.sqlite3
 pipenv run python manage.py migrate
